68cd0e0fb65731881221b44cf3165fd2e6af17ce
geertschneider <geert.schneider.be@gmail.com>
2020-05-11 15:57:01 +0200

    Show failing test

---
+++ InosStates/src/main/groovy/InosEvent.groovy
@@ -0,0 +1,15 @@
+package be.vdab.vdp
+
+class InosEvent {
+
+    int IKL
+    String eventName;
+
+    public InosEvent(int ikl,String eventName){
+        this.IKL=ikl
+        this.eventName=eventName
+    }
+
+
+}
+
--- InosStates/src/main/groovy/InosStateProcessor.groovy
+++ InosStates/src/main/groovy/InosStateProcessor.groovy
@@ -1,26 +1,53 @@
 package be.vdab.vdp
 
-import org.apache.kafka.streams.processor.Processor
+import be.vdab.vdp.InosEvent
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.stream.annotation.EnableBinding
 import org.springframework.cloud.stream.annotation.StreamListener
 import org.springframework.cloud.stream.messaging.Sink
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
 
 @SpringBootApplication
-@EnableBinding(Sink.class)
 class InosStateProcessor {
 
     static void main(String... args){
         SpringApplication.run(InosStateProcessor.class,args)
     }
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
+    @KafkaListener(topics = "STR_INOS_EVENTS_RAW", groupId = "myGroup3")
+    public void ConsumeInosRawEvents(String jsonMessage){
 
-    @StreamListener(Sink.INPUT)
-    void getIntegrationEvents(String inputMessage){
-        print("message ${inputMessage}")
+//        JsonSlurper slurper = new JsonSlurper()
+//        def fromJSON= slurper.parseText(jsonMessage)
+
+//        InosEvent ie = new InosEvent(inosEvent.IKL,inosEvent.EVENTNAME)
+//        ProduceOutput(ie)
+        println("got following message ${jsonMessage}")
+//        JsonSlurper slurper = new JsonSlurper()
+//        def fromJSON= slurper.parseText(jsonMessage)
+
     }
+
+    void ProduceOutput(InosEvent inosEvent){
+        println("sending to kafka")
+        this.kafkaTemplate.send("springOutput" , inosEvent.IKL.toString(),JsonOutput.toJson(inosEvent))
+    }
+
+
+
+//
+//    @StreamListener(Sink.INPUT)
+//    void getIntegrationEvents(LogMessage inputMessage){
+//        print("message ${inputMessage}")
+//    }
 
 
 }
