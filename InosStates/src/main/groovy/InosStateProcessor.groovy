d3d2834ae149668cb674698220f1f49dcd64ba87
geertschneider <geert.schneider.be@gmail.com>
2020-05-12 08:26:46 +0200

    Adding state processor

--- InosStates/src/main/groovy/InosStateProcessor.groovy
+++ InosStates/src/main/groovy/InosStateProcessor.groovy
@@ -1,54 +1,106 @@
-package be.vdab.vdp
+package be.vdab.vdp.InosStates
 
-import be.vdab.vdp.InosEvent
-import groovy.json.JsonOutput
+
 import groovy.json.JsonSlurper
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.SpringApplication
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.StreamsConfig
+import org.apache.kafka.streams.Topology
+import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.kstream.ValueTransformer
+import org.apache.kafka.streams.kstream.ValueTransformerSupplier
+import org.apache.kafka.streams.processor.ProcessorContext
+import org.apache.kafka.streams.state.KeyValueStore
+import org.apache.kafka.streams.state.StoreBuilder
+import org.apache.kafka.streams.state.Stores
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.cloud.stream.annotation.StreamListener
-import org.springframework.cloud.stream.messaging.Sink
-import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.core.KafkaTemplate
+
+import java.time.Duration
 
 @SpringBootApplication
 class InosStateProcessor {
 
-    static void main(String... args){
-        SpringApplication.run(InosStateProcessor.class,args)
+    static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "test-application");
+        props.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+
+        ValueTransformerSupplier<String,  Iterable<String>> valueTransformerSupplier = new ValueTransformerSupplier<String, Iterable<String>>() {
+
+            @Override
+            ValueTransformer<String, Iterable<String>> get() {
+                return new ValueTransformer<String,Iterable< String>>() {
+                    private KeyValueStore<Integer,String> store
+                    def jsonGenerator = new groovy.json.JsonGenerator.Options()
+                            .excludeFieldsByName("defaultEndDate")
+                            .build()
+
+                    public
+                    @Override
+                    void init(ProcessorContext context) {
+                        store =(KeyValueStore<Integer, String>) context.getStateStore("IKL")
+
+                    }
+
+
+                    @Override
+                    Iterable<String> transform(String value) {
+
+                        JsonSlurper slurper=new JsonSlurper()
+                        def fromJSON=slurper.parseText(value)
+                        def transFomredValues =[]
+                        int IKL = fromJSON.IKL
+
+                        def currentInosEvent = new InosEvent(IKL,fromJSON.EVENTIDENTIFIER,fromJSON.EVENTNAME,fromJSON.EVENTTIME,fromJSON.PAYLOAD)
+                        currentInosEvent.setEndDate()
+                        transFomredValues<< jsonGenerator.toJson(currentInosEvent)
+                        def prevEventText = store.get(IKL)
+                        if(prevEventText!=null)
+                        {
+                            def prevJson= slurper.parseText(prevEventText)
+                            def prevInosEvent = new InosEvent(IKL,prevJson.eventIdentifier,prevJson.eventName,prevJson.startDate,prevJson.endDate,prevJson.payload)
+                            prevInosEvent.setEndDate(currentInosEvent.startDate)
+                            transFomredValues<< jsonGenerator.toJson(prevInosEvent)
+                        }
+
+                        store.put(IKL,transFomredValues[0])
+
+                        return transFomredValues
+
+                    }
+
+
+
+                    @Override
+                    void close() {
+
+                    }
+                }
+            }
+        }
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        StoreBuilder storeBuilder = Stores.keyValueStoreBuilder(Stores.persistentKeyValueStore("IKL"),Serdes.Integer(),Serdes.String());
+        builder.addStateStore(storeBuilder);
+
+        KStream<String,String> stream = builder.stream("STR_INOS_NEWEVENTS", Consumed.with(Topology.AutoOffsetReset.EARLIEST));
+
+        stream.flatTransformValues(valueTransformerSupplier,"IKL").to("KafkaOutput");
+
+        Topology topo = builder.build();
+        KafkaStreams streams =new KafkaStreams(topo,props);
+        streams.start()
+
+        Runtime.getRuntime().addShutdownHook({
+            println("closing stream")
+            streams.close(Duration.ofSeconds(10))
+        })
     }
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
-
-    @KafkaListener(topics = "STR_INOS_EVENTS_RAW", groupId = "myGroup3")
-    public void ConsumeInosRawEvents(String jsonMessage){
-
-//        JsonSlurper slurper = new JsonSlurper()
-//        def fromJSON= slurper.parseText(jsonMessage)
-
-//        InosEvent ie = new InosEvent(inosEvent.IKL,inosEvent.EVENTNAME)
-//        ProduceOutput(ie)
-        println("got following message ${jsonMessage}")
-//        JsonSlurper slurper = new JsonSlurper()
-//        def fromJSON= slurper.parseText(jsonMessage)
-
-    }
-
-    void ProduceOutput(InosEvent inosEvent){
-        println("sending to kafka")
-        this.kafkaTemplate.send("springOutput" , inosEvent.IKL.toString(),JsonOutput.toJson(inosEvent))
-    }
-
-
-
-//
-//    @StreamListener(Sink.INPUT)
-//    void getIntegrationEvents(LogMessage inputMessage){
-//        print("message ${inputMessage}")
-//    }
-
 
 }
-
